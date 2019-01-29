835815629afa3af6a328fae73776520e50e5d723
wallygr97 <roodwally@gmail.com>
2019-01-29 14:27:33 -0400

    parte 3 de la tarea

--- src/main/java/com/gerard/Main.java
+++ src/main/java/com/gerard/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException{
@@ -21,11 +21,14 @@ public class Main {
 
         Document doc = Jsoup.connect(url).get();//consigue el documento html de la pagina y se conecta a ella
         int cant_Parrafos = doc.getElementsByTag("p").size();// te da el size de la cantidad de parrafos que tiene
+        int cantImgParrafos = doc.select("p img").size();// busca dentro de los parrafos todas las imagenes y nos da los size
 
          //punto 1
         System.out.println("la cantidad de linea que tiene la url son: "+ cantLineas + "\n");
         //punto 2
         System.out.println("la cantidad de parrafos que tiene son: "+ cant_Parrafos +"\n");
+        //punto 3
+        System.out.println("la cantidad de imagen en los parrafos son: "+ cantImgParrafos + "\n");
 
     }
 }
